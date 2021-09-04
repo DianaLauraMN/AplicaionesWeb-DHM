842cec31960948a47c6feb50efd7c34e2143d988
ManuelVzla03 <manuel.valenzuela216785@potros.itson.edu.mx>
2021-09-03 21:44:45 -0700

    I fixed the administrator and normal classes

--- BusinessObjects/src/businessObjects/Administrator.java
+++ BusinessObjects/src/businessObjects/Administrator.java
@@ -1,8 +1,11 @@
 package businessObjects;
 
+import java.util.Date;
+
 public class Administrator extends User {
 
-    public Administrator() {
+    public Administrator(String fullName, String email, String password, String avatar, String city, Date birthDay, char gender) {
+    super(fullName, email, password, avatar, city, birthDay, gender);
     
     }   
 }
--- BusinessObjects/src/businessObjects/Normal.java
+++ BusinessObjects/src/businessObjects/Normal.java
@@ -1,8 +1,10 @@
 package businessObjects;
 
+import java.util.Date;
+
 public class Normal extends User {
 
-    public Normal() {
-
+    public Normal(String fullName, String email, String password, String avatar, String city, Date birthDay, char gender) {
+        super(fullName, email, password, avatar, city, birthDay, gender);
     }
 }
