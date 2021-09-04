af5f413b9a6d7b5d87baf9394922c8a2a0a66df9
ManuelVzla03 <manuel.valenzuela216785@potros.itson.edu.mx>
2021-09-03 21:03:28 -0700

    The attributes and methods have been added to the abstract class User

--- BusinessObjects/src/businessObjects/Municipality.java
+++ BusinessObjects/src/businessObjects/Municipality.java
@@ -36,6 +36,33 @@ public class Municipality {
     }
 
     @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Municipality other = (Municipality) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    
+    @Override
     public String toString() {
         return "id=" + id + ", name=" + name + ", users=" + users;
     }
