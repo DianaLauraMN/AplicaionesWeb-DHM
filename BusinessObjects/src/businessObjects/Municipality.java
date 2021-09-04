db74f845987255409ba44b78ae99ba6a639efe16
ManuelVzla03 <manuel.valenzuela216785@potros.itson.edu.mx>
2021-09-03 20:59:15 -0700

    The  attributes and methods have been added to the Municipality class

--- BusinessObjects/src/businessObjects/Municipality.java
+++ BusinessObjects/src/businessObjects/Municipality.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package businessObjects;
 
-/**
- *
- * @author Manuel
- */
+import java.util.ArrayList;
+
 public class Municipality {
+    private int id;
+    private String name;
+    private ArrayList users = new ArrayList();
+
+    public Municipality() {
+   
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList getUsers() {
+        return users;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsers(ArrayList users) {
+        this.users = users;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id + ", name=" + name + ", users=" + users;
+    }
+    
     
 }
