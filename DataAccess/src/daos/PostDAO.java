5187b832d88b64d7610e718b57b6d42ca627e515
ManuelVzla03 <manuel.valenzuela216785@potros.itson.edu.mx>
2021-09-08 16:58:04 -0700

    The dao functions and methods of the posts class were successfully created.

--- DataAccess/src/daos/PostDAO.java
+++ DataAccess/src/daos/PostDAO.java
@@ -4,13 +4,16 @@ package daos;
 import businessObjects.Post;
 import com.mongodb.client.MongoCollection;
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.regex;
+import static daos.DAO.instance;
+import java.util.ArrayList;
 import java.util.List;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
 public class PostDAO implements DAO<Post> {
 
-     MongoCollection<Post> collection = instance.getConnection().getCollection("Posts", Post.class);
+     MongoCollection<Post> collection = instance.getConnection().getCollection("posts", Post.class);
 
     @Override
     public boolean insert(Post item) {
@@ -36,7 +39,24 @@ public class PostDAO implements DAO<Post> {
 
     @Override
     public boolean deleteItem(ObjectId idItem, ObjectId idDelete) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         Post posts = null;
+        try {
+            int x = -1;
+            posts = collection.find(eq("_id", idItem)).first();
+            List<ObjectId> idComments = posts.getComments();
+            for (int i = 0; i < idComments.size(); i++) {
+                if (idDelete.equals(idComments.get(i))) {
+                    x = i;
+                    break;
+                }
+            }
+            idComments.remove(x);
+            posts.setComments(idComments);
+            update(posts);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return true;
     }
 
     @Override
@@ -55,10 +75,8 @@ public class PostDAO implements DAO<Post> {
     @Override
     public Post find(ObjectId id) {
         Post post = null;
-        try {
-            
+        try {   
             post = collection.find(eq("_id", id)).first();
-
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
@@ -67,17 +85,39 @@ public class PostDAO implements DAO<Post> {
 
     @Override
     public List<Post> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         ArrayList<Post> posts = new ArrayList<>();
+        try {
+            collection.find().into(posts);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return posts;
     }
 
     @Override
     public List<Post> findLike(String pattern) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         ArrayList<Post> posts = new ArrayList<>();
+        try {
+            if (pattern.equalsIgnoreCase("")) {
+                return findAll();
+            } else {
+                collection.find(regex("name", pattern, "i")).into(posts);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return posts;
     }
 
     @Override
     public List<Post> findMany(int many) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       ArrayList<Post> posts = new ArrayList<>();
+        try {
+            collection.find().limit(many).into(posts);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return posts;
     }
 
 }
